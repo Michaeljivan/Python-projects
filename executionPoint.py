bb7817c065588793ec287ef9a93dea45f5c18cfb
Michael Jivan <32310825+Michaeljivan@users.noreply.github.com>
2018-11-08 08:14:25 -0800

    Create executionPoint.py

---
+++ executionPoint.py
@@ -0,0 +1,13 @@
+#Pyton
+
+
+#Function
+def main():
+	#start code here
+	print("Program execution point main:")
+	get_loop_count = int(raw_input("How many times: "))
+	for i in range(get_loop_count):
+		print(i)
+
+#Begin program
+main()
